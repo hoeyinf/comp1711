e865a585fb73ee9cec9b458f2d4c5756ebb859b4
Hoe Yin Foong <146224208+hoeyinf@users.noreply.github.com>
2023-10-16 11:33:02 +0000

    Week 3 Session 1 done

--- week3/session1/ws1ex5.c
+++ week3/session1/ws1ex5.c
@@ -2,27 +2,40 @@
 
 int main (){
     int choice, end = 0;
+
     while(end == 0){
-        printf("Enter 1 for an insult, 2 for a compliment, 3 for a fact, 4 for a greeting, and 5 for a farewell: \n");
+        printf("Enter one of the following:\n1: insult\n2: compliment\n3: fact\n4: greeting\n5: farewell\n");
         scanf("%d", &choice);
+
         switch(choice){
-            case 1: printf("You are ugly.\n");
-            end = 1;
+            case 1: 
+                printf("You are ugly.\n");
+                end = 1;
             break;
-            case 2: printf("You are beautiful.\n");
-            end = 1;
+
+            case 2: 
+                printf("You are beautiful.\n");
+                end = 1;
             break;
-            case 3: printf("A is the first letter of the alphabet.\n");
-            end = 1;
+
+            case 3: 
+                printf("A is the first letter of the alphabet.\n");
+                end = 1;
             break;
-            case 4: printf("Hello!\n");
-            end = 1;
+
+            case 4: 
+                printf("Hello!\n");
+                end = 1;
             break;
-            case 5: printf("Goodbye!\n");
-            end = 1;
+
+            case 5: 
+                printf("Goodbye!\n");
+                end = 1;
             break;
-            default: printf("Invalid input. ");
-     }
+
+            default: 
+                printf("Invalid input. ");
+        }
     }
     return 0;
 }
