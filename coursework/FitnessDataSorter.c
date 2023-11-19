a3edc8bca70a14db4529f8be446c44ebf5cbdc52
Hoe Yin Foong <146224208+hoeyinf@users.noreply.github.com>
2023-11-19 20:59:53 +0000

    Task 3 almost done. Need to add csv file verification

--- coursework/FitnessDataSorter.c
+++ coursework/FitnessDataSorter.c
@@ -1,5 +1,4 @@
-#include "FitnessDataStruct.h"
-#include <stdbool.h>
+#include "FitnessDataSorter.h"
 
 char filename[100], tempDate[11], tempTime[6];
 int total = 0, i, compareOne, compareTwo, tempSteps;
@@ -13,41 +12,24 @@ int main()
     FILE *file = fopen(filename, "r");
     if (!file)
     {
-        printf("Error: Could not find or open the file.\n");
+        printf("Error: invalid file\n");
         return 1;
     }
     // stores contents of file into the array, returns total number of records
     total = count_and_store(file, data);
     fclose(file);
-    while (sorted == false)
-    {
-        sorted = true;
-        compareOne = data[i].steps;
-        for (i = 0; i < total - 1; i++)
-        {
-            compareTwo = data[i + 1].steps;
-            if (compareTwo < compareOne)
-            {
-                tempSteps = compareOne;
-                strcpy(data[i].date, tempDate);
-                strcpy(data[i].time, tempTime);
-                data[i].steps = compareTwo;
-                data[i].date = data[i+1].date;
-                data[i].time = data[i+1].time;
-                data[i+1].steps = compareTwo;
-                data[i+1].date = tempDate;
-                data[i+1].time = tempTime;
-                sorted = false;
-            }
 
-            compareOne = data[i+1].steps;
-        }
-    }
+    sort_data(data, total);
 
-    FILE *filenew = fopen("FitnessData_2023.csv.tsv", "w");
+    // filename changed to have .tsv at the end of it
+    strcat(filename, ".tsv");
+    // creates file, writes contents of sorted array into it (tsv format)
+    FILE *filenew = fopen(filename, "w");
     for (i = 0; i < total; i++)
     {
         fprintf(filenew, "%s\t%s\t%d\n", data[i].date, data[i].time, data[i].steps);
-        fclose(filenew);
     }
+    fclose(filenew);
+    printf("Data sorted and written to %s\n", filename);
+    return 0;
 }
