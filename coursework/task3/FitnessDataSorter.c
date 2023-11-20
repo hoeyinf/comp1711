36cf8d2d8dd1cb86c2ed1b2e78b54880ac88dc83
Hoe Yin Foong <146224208+hoeyinf@users.noreply.github.com>
2023-11-20 21:42:31 +0000

    Added comments to task 3

--- coursework/task3/FitnessDataSorter.c
+++ coursework/task3/FitnessDataSorter.c
@@ -7,27 +7,31 @@ FITNESS_DATA data[200];
 
 int main()
 {
+    // user inputs filename, error if file doesn't exist
     printf("Enter Filename: ");
     scanf("%s", filename);
-    while (getchar() != '\n')
-        ;
+    while (getchar() != '\n');
     FILE *file = fopen(filename, "r");
     if (!file)
     {
         printf("Error: invalid file\n");
         return 1;
     }
+
+    // function will check for correct file format while reading/storing data
+    // returns true if file is invalid
     error = validate_store_count(file, data, &records);
+    fclose(file);
     if (error == true)
     {
         printf("Error: invalid file\n");
-        fclose(file);
         return 1;
     }
-    fclose(file);
 
+    // sorts data, using bubble sort
     sort_data(data, records);
 
+    // creates appropriately named tsv file
     output_tsv(filename, data, records);
 
     return 0;
